076cf0489d31af34dcd72f176d5a25e96a7322a3
WaterDess <waterdess2025@gmail.com>
2026-06-28 06:33:04 +0800

    Compact people page layout

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
   const app = document.getElementById("app");
   const page = document.body.dataset.page || "home";
   const personSlug = document.body.dataset.person || "";
-  const SITE_VERSION = "toc-nav-3";
+  const SITE_VERSION = "people-compact-1";
 
   const nav = [
     ["news", "News"],
